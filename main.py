9a7621a36d827118296fc79b3051da7b681332c3
c4llv07e <igor@c4llv07e.xyz>
2025-08-02 10:08:11 +0300

    More flexible

--- main.py
+++ main.py
@@ -1,40 +1,39 @@
 #!/usr/bin/env python3
+import subprocess
 from flask import request, Flask, render_template, redirect, url_for
 import os
 import sys
 import time
 from urllib.parse import urlencode
 
-program_name = "aoeu"
-title = "Adventure space server management"
-
-app = Flask(__name__, static_url_path='')
-
 def eprint(*args, **kwargs):
     return print(*args, file=sys.stderr, **kwargs)
 
-def spawn(file, args):
-    pid = os.fork()
-    if pid == -1:
-        eprint("can't fork to execute", file)
-        return -1
-    if pid == 0:
-        os.execvp(file, args)
-        # What you even do in failed to exec, but forked app?
-        eprint("can't execvp", file)
-        os.exit(-1)
-    return os.waitpid(pid, 0)[1]
+def get_env_or_exit(env):
+    if env not in os.environ:
+        eprint(f"Error, {env} is unset")
+        sys.exit(-1)
+    return os.environ[env]
 
-@app.route("/control", methods=["POST"])
+program_name = get_env_or_exit("SERVICE_NAME")
+title = os.getenv("TITLE", "Service monitoring")
+
+app = Flask(__name__, static_url_path='')
+
+@app.route("/", methods=["POST"])
 def route_control():
     action = request.form.get("action")
     if action is None:
         return redirect("/?" + urlencode(values={"message": "wrong action"}))
     if action == "restart":
-        status = spawn("zenity", ["zenity", "--info", "--text", program_name])
-        # status = spawn("systemctl", ["systemctl", "restart", program_name])
-        if status == 0:
-            return redirect("/?" + urlencode({"message": "service restarted"}))
+        try:
+            process = subprocess.run(["systemctl", "restart", program_name])
+            status = process.returncode
+            if status == 0:
+                return redirect("/?" + urlencode({"message": "service restarted"}))
+        except FileNotFoundError:
+            eprint("can't run systemctl")
+            status = -1
         return redirect("/?" + urlencode({"message": f"error on restarting service, status code: {status}"}))
     return redirect("/?" + urlencode({"message", "unknown action"}))
 
